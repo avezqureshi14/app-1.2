8515610cd2bf2b47041a66529937303351ef50bf
avezqureshi14 <avez@webknot.in>
2025-03-16 17:24:27 +0530

    deployment : _redirects netlify

--- src/components/custom/BaseChart.vue
+++ src/components/custom/BaseChart.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="w-[200px] md:w-1/2 p-4 bg-white rounded-lg shadow-md">
+    <div class="w-[200px] md:w-1/2 p-4 ">
         <h2 class="text-lg font-semibold text-center">{{ title }}</h2>
         <apexchart :type="type" :options="config" :series="series"></apexchart>
         <slot name="controls"></slot>
